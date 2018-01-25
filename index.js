76062dadc6d2e290b72739da9509be1a28094915
jscarton <jscarton@gmail.com>
2018-01-25 17:01:30 -0500

    new status command

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ var program = require('commander');
 var chalk = require('chalk');
 
 program
-  .version(chalk.cyan('INFO:')+"ADE CLI Version: 0.0.1 Codename: LA PELUA\nby jscarton. Learn more at: https://github.com/jscarton/arepas-commander")
+  .version(chalk.cyan('INFO:')+"ADE CLI Version: 0.0.2 Codename: LA PELUA\nby jscarton. Learn more at: https://github.com/jscarton/arepas-commander")
   .description('Arepas Commander CLI')
-  .command('init <platform>', 'creates arepas.json file with all data required about your ADE')
-  .command('start <platform>', 'start platform\'s ADE, is this is the first time the ADE will be setup and provisioned')
-  .command('stop <platform>', 'stop platform\'s ADE if running')
-  .command('reload <platform>', 'restart platform\'s ADE if it is running')
-  .command('connect <platform>', 'open an ssh connection to platform\'s ADE if it is running')
-  .command('delete <platform>', 'destroy a platform\'s ADE')
+  .command('init <platform> <project>', 'initialize an ADE in the folder <project> using the chosen <platform>')
+  .command('start <project>', 'start and ADE, is this is the first time the ADE will be setup and provisioned')
+  .command('stop <project>', 'stop a a running ADE')
+  .command('reload <project>', 'restart a running ADE')
+  .command('connect <project>', 'open an ssh connection to a running ADE')
+  .command('delete <project>', 'destroy an ADE')
+  .command('status <pproject>', 'check status of an ADE')
   .parse(process.argv);
---
+++ status/index.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+var program = require('commander');
+var shell = require('shelljs');
+var fs = require('fs');
+var chalk = require('chalk');
+
+//get current working directory
+var cwd=process.cwd();
+
+//parse commands args and options
+
+program
+    .arguments('<project>')
+	.parse(process.argv);
+
+var project=program.args[0];
+
+if (project==undefined){
+  console.log(chalk.red('ERROR:')+'missing argument <project>');
+  shell.exit(1);
+}
+
+//check Vagrant is installed
+check_vagrant=shell.which('vagrant');
+if (!check_vagrant) {
+  console.log(chalk.red('ERROR:')+'sorry, ADE requires vagrant installed on host machine');
+  shell.exit(1);
+}
+else
+	console.log(chalk.green('SUCCESS:')+ 'vagrant found at '+check_vagrant);
+
+//check Virtual box is installed
+check_vbox=shell.which('VBoxManage');
+if (!check_vbox) {
+  console.log(chalk.red('ERROR:')+'sorry, ADE requires VirtualBox installed on host machine');
+
+  shell.exit(1);
+}
+else
+	console.log(chalk.green('SUCCESS:')+ 'VirtualBox found at '+check_vbox);
+
+console.log(chalk.cyan('INFO:')+ 'querying status of Arepas Developer Environment (ADE) at '+cwd+'/'+project);
+
+//Setup ADE specific platform
+try {
+  fs.accessSync(project, fs.constants.R_OK);
+  shell.cd(project);
+  if (shell.exec("vagrant status").code !== 0) {
+    console.log(chalk.red('ERROR:')+'sorry, vagrant has returned a non zero exit code, please check output to see what happened');
+    shell.exit(1);
+  }
+} catch (e) {
+    console.log(chalk.cyan('ERROR:')+ 'trying to get status of ADE for '+project.toUpperCase()+", please ensure you have initialized this ADE");
+}
